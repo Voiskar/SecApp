f9a07c7925371825693d092a5dbe848dd75017e7
voiskar <eanitis@hotmail.com>
2022-05-23 23:21:03 +0300

    Add project files.

---
+++ SecApp/Controllers/admin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
